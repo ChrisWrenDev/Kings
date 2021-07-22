9d606f1b188fa48a942b2399dea86135e546c5cd
Chris Wren <dazedpandas@gmail.com>
2021-07-22 13:10:25 +0100

    Hero UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Hero />
     </Fragment>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,20 @@
 import classes from "./Hero.module.css";
+import backgoundImg from "../../assets/home-background.jpg";
+import logoBadge from "../../assets/Home_Badge--White.svg";
 
 const Hero = () => {
-  return;
+  return (
+    <div
+      className={classes.hero}
+      style={{
+        backgroundImage: `url(${backgoundImg})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <img className={classes.logo} src={logoBadge} alt="logo badge" />
+    </div>
+  );
 };
 
 export default Hero;
